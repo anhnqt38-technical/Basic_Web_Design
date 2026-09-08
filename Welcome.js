74b6bfa62080f272026811d5acd1daf2d1a38906
Nguyen Quang Tuan Anh <anhnqt38@gmail.com>
2026-09-08 19:39:08 +0700

    Homework in 08/09/2026

---
+++ Welcome.js
@@ -0,0 +1,10 @@
+// alert("Welcome to My Website!");
+console.log("Welcome to My Website!");
+
+let ten = "Tuan Anh";
+console.log("Xin chào " + ten + "!");
+
+let a = 10;
+let b = 30;
+
+console.log("Tổng của a và b là: " + (a + b));
